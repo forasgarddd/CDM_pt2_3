40099b70f1274e670e2a8c21d22f905f589c96d7
Ivan Devitskyi <forbattlenetonly14@gmail.com>
2020-12-02 11:40:51 +0200

    some minor commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # F = 10110000
 
+# return F
 def func_f():
     if x == 0 and y == 0 and z == 0:
         return 1
@@ -19,6 +20,7 @@ def func_f():
         return 0
 
 
+# Zhegalkin polynomial
 def linear():
     global a0, a2, a3, a1, a12, a23, a13, a123
     af = func_f()
@@ -48,6 +50,7 @@ def linear():
         return a123
 
 
+# check for linear by Zhegalkin polynomial
 def is_linear():
     if a12 == 0 and a13 == 0 and a23 == 0 and a123 == 0:
         return "Функція лінійна"
@@ -55,17 +58,19 @@ def is_linear():
         return "Функція нелінійна"
 
 
-def check_0_const():
-    if x == 0 and y == 0 and z == 0 and (func_f() == 0):
+def saves_0_const():
+    af = func_f()
+    if x == 0 and y == 0 and z == 0 and (af == 0):
         return 1
-    if x == 0 and y == 0 and z == 0 and (func_f() == 1):
+    if x == 0 and y == 0 and z == 0 and (af == 1):
         return 0
 
 
-def check_1_const():
-    if x == 1 and y == 1 and z == 1 and (func_f() == 1):
+def saves_1_const():
+    af = func_f()
+    if x == 1 and y == 1 and z == 1 and (af == 1):
         return 1
-    if x == 1 and y == 1 and z == 1 and (func_f() == 0):
+    if x == 1 and y == 1 and z == 1 and (af == 0):
         return 0
 
 
@@ -113,51 +118,61 @@ print("x y z  F")
 for x in 0, 1:
     for y in 0, 1:
         for z in 0, 1:
+            # print truth table
             print(x, y, z, "", func_f())
-            if check_0_const() == 1:
+            if saves_0_const() == 1:
                 saves_0 = "Функція зберігає константу 0"
-            if check_0_const() == 0:
+            if saves_0_const() == 0:
                 saves_0 = "Функція не зберігає константу 0"
-            if check_1_const() == 1:
+            if saves_1_const() == 1:
                 saves_1 = "Функція зберігає константу 1"
-            if check_1_const() == 0:
+            if saves_1_const() == 0:
                 saves_1 = "Функція не зберігає константу 1"
             if ddnf() != None:
                 ddnf_array_e.append(ddnf())
                 ddnf_array_e.append("\/")
+                if len(ddnf_array_e) != 0:
+                    ddnf_array_e.pop()
             if dknf() != None:
                 dknf_array_e.append(dknf())
                 dknf_array_e.append("/\\")
+                if len(dknf_array_e) != 0:
+                    dknf_array_e.pop()
             f_arr.append(str(func_f()))
-            # print(linear())
             linear_array.append(linear())
 
-f = ''.join(f_arr)
 
-
-# print(*linear_array)
-# print("a12:", a12, "a13:", a13,"a23:", a23, "a123:", a123)
-
-
-def inversed(f):
+def inverse():
+    f = ''.join(f_arr)
     f = list(f)
     for i in range(len(f)):
         f[i] = '1' if f[i] == '0' else '0'
     return ''.join(f)
 
 
-def is_self_dual(f):
-    if f == ''.join(reversed(inversed(f))):
+def is_self_dual():
+    f = ''.join(f_arr)
+    if f == ''.join(reversed(inverse())):
         return "Функція самодвоїста"
     else:
         return "Функція не самодвоїста"
 
 
-print("1.", saves_0)
-print("2.", saves_1)
-print("3.", is_self_dual(f))
-print("4.", is_linear())
-ddnf_array_e.pop()
-print("5.", "ДДНФ:", *ddnf_array_e)
-dknf_array_e.pop()
-print("6.", "ДКНФ:", *dknf_array_e)
+# optional check for linear
+# print("Поліном Жегалкіна: ", "a0:", a0, "a1:", a1, "a2:", a2, "a3:", a3, "a12:", a12, "a13:", a13,"a23:", a23, "a123:", a123)
+
+# Function characteristics output
+def output():
+    print("1.", saves_0)
+    print("2.", saves_1)
+    print("3.", is_self_dual())
+    print("4.", is_linear())
+    if len(ddnf_array_e) != 0:
+        ddnf_array_e.pop()
+    if len(dknf_array_e) != 0:
+        dknf_array_e.pop()
+    print("5.", "ДДНФ:", *ddnf_array_e)
+    print("6.", "ДКНФ:", *dknf_array_e)
+
+
+output()
